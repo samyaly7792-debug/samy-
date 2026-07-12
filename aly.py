fb5b60548509d4a30ec2bd5fa283134b1fef9cf7
samyaly7792-debug <samyaly779.2@gmail.com>
2026-07-12 04:34:32 +0300

    Update aly.py

--- aly.py
+++ aly.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit, disconnect
+ flask_socketio import SocketIO, emit, disconnect
 import os
 
 app = Flask(__name__)
