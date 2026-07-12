f160273d7442b40a7d33da3b6a21c4bdfc18b91e
samyaly7792-debug <samyaly779.2@gmail.com>
2026-07-12 03:54:48 +0300

    Update aly.py

--- aly.py
+++ aly.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, disconnect
 import os
-import random
 
 app = Flask(__name__)
+# يُفضل دائماً تغيير مفتاح التشفير ليكون أكثر أماناً في الإنتاج
 app.config['SECRET_KEY'] = 'samy_king_final_2026'
-socketio = SocketIO(app, cors_allowed_origins="*")
+# إعداد SocketIO مع السماح بجميع المصادر للاتصال
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode=None)
 
 active_users = {}
 ADMIN_NAME = "المهندس"
@@ -19,20 +20,22 @@ def index():
 def on_join(data):
     name = data.get('name', 'زائر')
     password = data.get('pass', '')
+    
+    # التحقق من صلاحيات المدير
     is_admin = (name == ADMIN_NAME and password == ADMIN_PASS)
+    
+    # حفظ المستخدم في قاموس المستخدمين النشطين
     active_users[request.sid] = {'name': name, 'is_admin': is_admin}
     
+    # إرسال تأكيد الانضمام للعميل (لإخفاء شاشة الدخول)
+    emit('join_success', {'status': 'success'}, room=request.sid)
+    
+    # إرسال رسالة ترحيبية للجميع
     welcome_msg = f"دخل المالك {name}" if is_admin else f"دخل {name} بإشراف المهندس"
     emit('status', {'msg': welcome_msg}, broadcast=True)
+    
+    # تحديث قائمة المستخدمين للجميع
     emit('user_list', active_users, broadcast=True)
-
-@socketio.on('kick_user')
-def on_kick(data):
-    target_sid = data.get('target_sid')
-    if active_users.get(request.sid, {}).get('is_admin'):
-        if target_sid in active_users:
-            emit('kicked', {'msg': 'تم طردك من قبل المالك'}, room=target_sid)
-            disconnect(sid=target_sid)
 
 @socketio.on('text')
 def on_text(data):
@@ -41,7 +44,7 @@ def on_text(data):
         emit('message', {
             'sender': user['name'],
             'is_admin': user['is_admin'],
-            'msg': data['msg']
+            'msg': data.get('msg', '')
         }, broadcast=True)
 
 @socketio.on('disconnect')
@@ -53,5 +56,6 @@ def on_disconnect():
         emit('user_list', active_users, broadcast=True)
 
 if __name__ == '__main__':
+    # استخدام المنفذ المخصص من بيئة الاستضافة أو الافتراضي 5000
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='0.0.0.0', port=port)
