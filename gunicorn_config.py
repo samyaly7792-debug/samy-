e60ac8f82a6bc8385c77af71564990c7478dec59
samyaly7792-debug <samyaly779.2@gmail.com>
2026-07-14 05:16:57 +0300

    Delete gunicorn_config.py

--- gunicorn_config.py
+++
@@ -1,9 +0,0 @@
-# gunicorn_config.py
-import multiprocessing
-
-# إعدادات Gunicorn مع gevent (بديل أفضل لـ eventlet)
-workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"  # استخدام gevent بدلاً من eventlet
-bind = "0.0.0.0:$PORT"
-timeout = 120
-keepalive = 5
